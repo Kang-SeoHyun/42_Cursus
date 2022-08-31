767513a43c334f7f5019a9dd61aeb65ec34abc5e
Seohyun Kang <seokang@c1r8s6.42seoul.kr>
2022-08-31 18:59:18 +0900

    hi

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,4 +1,107 @@
-char *get_next_line(int fd)
-{
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokang <seokang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 16:32:48 by seokang           #+#    #+#             */
+/*   Updated: 2022/08/31 18:43:36 by seokang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*renew(char *str)
+{
+	int		i;
+	int		j;
+	char	*renewed;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str[i])
+	{
+		free(str);
+		return (NULL);
+	}
+	renewed = (char *)malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	if (!renewed)
+		return (NULL);
+	i++;
+	j = 0;
+	while (str[i])
+		renewed[j++] = str[i++];
+	renewed[j] = '\0';
+	return (renewed);
+}
+
+char	*first_line(char *str)
+{
+	int		i;
+	char	*first;
+
+	i = 0;
+	if (!str[i])
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	first = (char *)malloc(sizeof(char) * (i + 2));
+	if (!first)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		first[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		first[i] = str[i];
+		i++;
+	}
+	first[i] = '\0';
+	return (first);
+}
+
+char	*save_all(int fd, char *str)
+{
+	char	*buf;
+	int		num;
+
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	num = 1;
+	while (!ft_strchr(str, '\n') && num)
+	{
+		num = read(fd, buf, BUFFER_SIZE);
+		if (num == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		if (num == 0)
+			break ;
+		buf[num] = '\0';
+		str = ft_strjoin(str, buf);
+	}
+	free(buf);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*first;
+	static char	*str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	str = save_all(fd, str);
+	if (!str)
+		return (NULL);
+	first = first_line(str);
+	str = renew(str);
+	return (first);
 }
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokang <seokang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 16:32:43 by seokang           #+#    #+#             */
+/*   Updated: 2022/08/31 17:22:39 by seokang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
