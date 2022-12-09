fdba65e29206b0f9c346439512d84268679d2d1e
seohyun <kang061020@gmail.com>
2022-12-09 20:17:50 +0900

    hi

--- push_swap/mandatory/push_swap.h
+++ push_swap/mandatory/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: seokang <seokang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 21:55:32 by seokang           #+#    #+#             */
-/*   Updated: 2022/12/07 20:50:16 by seokang          ###   ########.fr       */
+/*   Updated: 2022/12/09 18:12:49 by seokang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 # include <stdlib.h>
 # include <fcntl.h>
 
-typedef struct	s_node
+typedef struct s_node
 {
-	int				data;
 	struct s_node	*up;
+	int				data;
 	struct s_node	*down;
 }	t_node;
 
@@ -30,13 +30,23 @@ typedef struct s_stack
 	struct s_node	*bottom;
 }	t_stack;
 
-typedef struct s_var
+typedef struct s_info
 {
 	struct s_stack	*stack_a;
+	int				size_a;
 	struct s_stack	*stack_b;
-}	t_var;
+	int				size_b;
+	int				*array;
+}	t_info;
 
-// stack.c
-void	init(t_var *var);
+// stack_make.c
+void	init(t_info *var);
+t_node	*get_new_node(int num);
+
+// push_pop.c
+void	push_top(t_stack *stack, t_node *new);
+void	push_bottom(t_stack *stack, t_node *new);
+t_node	*pop_top(t_stack *stack);
+t_node	*pop_bottom(t_stack *stack);
 
 #endif
--- push_swap/mandatory/stack_make.c
+++ push_swap/mandatory/stack_make.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   .c                                                 :+:      :+:    :+:   */
+/*   stack_make.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seokang <seokang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 22:04:19 by seokang           #+#    #+#             */
-/*   Updated: 2022/12/07 20:35:28 by seokang          ###   ########.fr       */
+/*   Updated: 2022/12/09 18:10:15 by seokang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	init_stack(t_var *var)
+void	init_stack(t_info *info)
 {
-	var->stack_a = (t_stack *)malloc(sizeof(t_stack));
-	var->stack_b = (t_stack *)malloc(sizeof(t_stack));
-	var->stack_a->top = get_new_node(0);
-	var->stack_a->bottom = get_new_node(0);
-	var->stack_a->top->down = var->stack_a->bottom;
-	var->stack_a->bottom->up = var->stack_a->top;
-	var->stack_b->top = get_new_node(0);
-	var->stack_b->bottom = get_new_node(0);
-	var->stack_b->top->down = var->stack_b->bottom;
-	var->stack_b->bottom->up = var->stack_b->top;
+	info->stack_a = (t_stack *)malloc(sizeof(t_stack));
+	info->stack_b = (t_stack *)malloc(sizeof(t_stack));
+	info->stack_a->top = get_new_node(0);
+	info->stack_a->bottom = get_new_node(0);
+	info->stack_a->top->down = info->stack_a->bottom;
+	info->stack_a->bottom->up = info->stack_a->top;
+	info->stack_b->top = get_new_node(0);
+	info->stack_b->bottom = get_new_node(0);
+	info->stack_b->top->down = info->stack_b->bottom;
+	info->stack_b->bottom->up = info->stack_b->top;
 }
 
 t_node	*get_new_node(int num)
