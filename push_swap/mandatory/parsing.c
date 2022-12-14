1c75f6c6837f08490e23956a4a3e6df5bb7c2b8c
seohyun <kang061020@gmail.com>
2022-12-14 16:54:04 +0900

    bye..

--- push_swap/mandatory/parsing.c
+++ push_swap/mandatory/parsing.c
@@ -1,28 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seokang <seokang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/10 12:13:32 by seokang           #+#    #+#             */
-/*   Updated: 2022/12/14 16:09:27 by seokang          ###   ########.fr       */
+/*   Created: 2022/12/14 16:17:08 by seokang           #+#    #+#             */
+/*   Updated: 2022/12/14 16:32:13 by seokang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char *av[])
-{
-	t_info	*stack_info;
-
-	if (ac < 2)
-		print_error();
-	else
+void	validate_args(int ac, char *av[], t_info *stack_info){
+	int	i;
+	
+	i = 1;
+	while (i < av)
 	{
-		init_stack(&stack_info);
-		validate_args(ac, av, &stack_info);
-		exit(0);
-	}	
-	return (0);
+    	int	j;
+		
+		j = 0;
+    	while (av[i][j])
+    	{
+        	if argv[i][j]
+        	{
+				char	*from;
+				char	*to;
+            	from = argv[i][j];
+            	while (argv[i][j])
+				j++;
+            	to = argv[i][j];
+            	ft_atoi(from, to);
+        	}
+			j++;
+    	}
+		i++
+	}
 }
